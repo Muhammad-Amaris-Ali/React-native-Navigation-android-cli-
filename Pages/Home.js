3294a1f85289aae5f95c3fd43c746896e4d32494
Muhammad-Amaris-Ali <amarisali221@gmail.com>
2022-05-31 16:01:09 +0500

    SettingsUpdate

--- Pages/Home.js
+++ Pages/Home.js
@@ -1,14 +1,15 @@
 import * as React from 'react';
-import {StyleSheet,
-   Button,
-    View
-    , Text 
-    ,TouchableOpacity
-    ,ImageBackground 
-    ,ScrollView
-    , Image,
-  
-  } from 'react-native';
+import {
+  StyleSheet,
+  Button,
+  View
+  , Text
+  , TouchableOpacity
+  , ImageBackground
+  , ScrollView
+  , Image,
+
+} from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -20,56 +21,55 @@ const image5 = { uri: "https://o.remove.bg/downloads/3ea1693b-214c-411a-bc76-8e3
 const Home = ({ navigation }) => {
   return (
     <View style={styles.main_dome}>
-          <View style={styles.div_1}>
+      <View style={styles.div_1}>
+        <TouchableOpacity onPress={() => navigation.openDrawer()}>
+          <Text style={styles.topictop}>Wallet</Text></TouchableOpacity>
 
-      <Text style={styles.topictop}>Wallet</Text>
-
-      <Image
-        style={styles.image3}
-        source={image3} />
+        <Image
+          style={styles.image3}
+          source={image3} />
       </View>
 
-<View style={styles.div_2}>
-<ImageBackground source={image4}  resizeMode="cover" style={styles.imageback2}>
-<View style={styles.div_001_inn}><View style={styles.div_3_inn}>
-  <Text style={styles.text_3_inn}>Balance</Text> 
-      <Image
-        style={styles.image5}
-        source={image5} />
-</View>
-<View style={styles.div_4_inn}>
-  <Text style={styles.text_5_inn}>$ 16,875</Text> 
-    
-</View>
-<View style={styles.div_004_inn}>
-<Text style={styles.text_004_inn}>$16,875</Text> 
-<Text style={styles.text_004_inn}>$16,875</Text> 
-<Text style={styles.text_004_inn}>$16,875</Text> 
-<Text style={styles.text_004_inn}>$16,875</Text> 
-</View>
-<View style={styles.div_004_inn}>
-<Text style={styles.text_004_inn}>$16,875</Text> 
-<Text style={styles.text_004_inn}>$16,875</Text> 
-<Text style={styles.text_004_inn}>$16,875</Text> 
-<Text style={styles.text_004_inn}>$16,875</Text> 
-</View>
-<View style={styles.div_004_inn}>
-<Text style={styles.text_004_inn}>$16,875</Text> 
-<Text style={styles.text_004_inn}>$16,875</Text> 
-<Text style={styles.text_004_inn}>$16,875</Text> 
-<Text style={styles.text_004_inn}>$16,875</Text> 
-</View>
-</View>
-</ImageBackground>
+      <View style={styles.div_2}>
+        <ImageBackground source={image4} resizeMode="cover" style={styles.imageback2} imageStyle={{ borderRadius: 20 }}>
+          <View style={styles.div_001_inn}><View style={styles.div_3_inn}>
+            <Text style={styles.text_3_inn}>Balance</Text>
+            <Image
+              style={styles.image5}
+              source={image5} />
+          </View>
+              <Text style={styles.text_5_inn}>$ 16,875</Text>
 
-</View>
+           
+            <View style={styles.div_004_inn}>
+              <Text style={styles.text_004_inn}>$16,875</Text>
+              <Text style={styles.text_004_inn}>$16,875</Text>
+              <Text style={styles.text_004_inn}>$16,875</Text>
+              <Text style={styles.text_004_inn}>$16,875</Text>
+            </View>
+            <View style={styles.div_004_inn}>
+              <Text style={styles.text_004_inn}>$16,875</Text>
+              <Text style={styles.text_004_inn}>$16,875</Text>
+              <Text style={styles.text_004_inn}>$16,875</Text>
+              <Text style={styles.text_004_inn}>$16,875</Text>
+            </View>
+            <View style={styles.div_004_inn}>
+              <Text style={styles.text_004_inn}>$16,875</Text>
+              <Text style={styles.text_004_inn}>$16,875</Text>
+              <Text style={styles.text_004_inn}>$16,875</Text>
+              <Text style={styles.text_004_inn}>$16,875</Text>
+            </View>
+          </View>
+        </ImageBackground>
+
+      </View>
 
       <Button
         title="Go to Details"
         onPress={() => navigation.navigate('Details')}
       />
 
-      
+
 
     </View>
   )
@@ -78,93 +78,99 @@ const Home = ({ navigation }) => {
 export default Home
 
 const styles = StyleSheet.create({
-  main_dome:{
-    backgroundColor:"white"
+  main_dome: {
+    backgroundColor: "white"
+    ,
+    width:"100%"
   },
-  div_1:{
-    display:"flex",
-    flexDirection:"row",
-    alignItems:"center",
-    justifyContent:"space-between",
-    width:"100%",
-     textAlign:"center",
-     marginTop:25,
-     marginLeft:-20
+  div_1: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "100%",
+    textAlign: "center",
+    marginTop: 25,
+    marginLeft: -20
 
   },
-  topictop:{
-    fontWeight:"bold",
-    fontSize:18,
-    marginLeft:"45%"
+  topictop: {
+    fontWeight: "bold",
+    fontSize: 18,
+    marginLeft: "45%"
   },
-  image3:{
-    width:60,
-    height:60
+  image3: {
+    width: 60,
+    height: 60
   },
-  div_2:{
-    display:"flex",
-    flexDirection:"row",
-    alignItems:"center",
-    justifyContent:"center",
-    height:"55%",
-    width:"100%",
-    padding:30,
+  div_2: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "55%",
+    width: "100%",
+    padding: 30,
     // borderRadius:30
   },
-  imageback2:{
-    display:"flex",
-    flexDirection:"row",
-    alignItems:"center",
-    justifyContent:"center",
-    height:"100%",
-    width:"100%"
- 
+  imageback2: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100%",
+    width: "100%",
+
+
   },
-  div_001_inn:{
-     display:"flex",
-    flexDirection:"column",
-    alignItems:"center",
-  
+  div_001_inn: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width:"100%"
+
   }
   ,
-  image5:{
-    width:60,
-    height:30
+  image5: {
+    width: 60,
+    height: 30
   },
-  div_3_inn:{
-    display:"flex",
-    flexDirection:"row",
-    alignItems:"center",
-    justifyContent:"space-evenly",
-    width:"100%"
+  div_3_inn: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "60%"
 
-  },div_4_inn:{
+  }, div_4_inn: {
 
   },
-  text_5_inn:{
-    marginLeft:-140,
-    color:"white",
-    fontWeight:"bold",
-    fontSize:30
+  text_5_inn: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 30
   },
-  text_3_inn:{
-    color:"white",
-    fontWeight:"bold",
-    fontSize:14
+  text_3_inn: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 14
     ,
-    marginLeft:-60
+    marginLeft: -60
   },
-  div_004_inn:{
-    display:"flex",
-    flexDirection:"row",
-    alignItems:"center",
-    justifyContent:"space-evenly",
+  div_004_inn: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+    width: "100%"
+
   },
-  text_004_inn:{
-    color:"white",
-    fontWeight:"bold",
-    fontSize:14,
-    marginLeft:12
+  text_004_inn: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 14,
+    // marginLeft:12
   }
 
 
--- Pages/Settings.js
+++ Pages/Settings.js
@@ -1,14 +1,138 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Button,
+  View
+  , Text
+  , TouchableOpacity
+  , ImageBackground
+  , ScrollView
+  , Image,
 
-const Settings = () => {
+} from 'react-native';import React from 'react'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+const imageicon1 = { uri: "https://o.remove.bg/downloads/03fbe751-ec1b-40cc-9b5e-878571ad15d2/bars-1440391-1216351-removebg-preview.png" }
+const image63 = { uri: "https://i.pinimg.com/originals/b8/69/5f/b8695f007aea9a08a0419479217ca6aa.jpg" }
+const image64 = { uri: "https://images.wallpapersden.com/image/download/gradient-rainbow-5k_a2psZmiUmZqaraWkpJRsa21lrWloZ2U.jpg" }
+const image65 = { uri: "https://o.remove.bg/downloads/3ea1693b-214c-411a-bc76-8e3f680f2916/Mastercard-Logo-removebg-preview.png" }
+
+const Settings = ({ navigation }) => {
   return (
-    <View>
-      <Text>Settings</Text>
+    <View style={{backgroundColor:"white"}}>
+       <View style={styles.div_1_2} >
+       <TouchableOpacity onPress={() => navigation.openDrawer()}>
+    <Image
+          style={styles.imageicon1}
+          source={imageicon1} />
+          </TouchableOpacity>
+      <Text  style={styles.Text_0021}>Wallet</Text>
+      <Image
+          style={styles.image63}
+          source={image63} />
+      
     </View>
+    <ScrollView style={{height:hp("100%")}}>
+    <View style={styles.maindome_2}>
+   
+    <ImageBackground style={styles.div_1_3} source={image64} resizeMode="cover"  imageStyle={{ borderRadius: 20 }} >
+    <View style={styles.div_1_4}>
+    <View style={styles.div_1_5}>
+      <Text style={styles.Text_0022} >Balance</Text>
+      <Image style={styles.image65}
+              source={image65}/>
+    </View>
+    <Text style={styles.Text_0023}> $165,000</Text>
+    <Text style={styles.Text_0024}>784 654 658 651</Text>
+    </View>
+
+    </ImageBackground>
+
+    </View>
+    </ScrollView>
+    </View>
+
   )
 }
 
 export default Settings
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+maindome_2:{
+  display:"flex",
+  flexDirection:"column",
+  alignItems:"center",
+  justifyContent:"center"
+  ,
+  // marginTop:40,
+
+  width:wp("100%"),
+  // height:hp("100%"),
+  backgroundColor:"white"
+},
+div_1_2:{
+  backgroundColor:"white",
+  display:"flex",
+  flexDirection:"row",
+  alignItems:"center",
+  justifyContent:"space-between",
+  width:wp("100%"),
+  padding:20,
+  height:60
+},
+imageicon1:{
+  height:30,
+  width:30
+},
+image63: {
+  width: 50,
+  height: 50
+},
+Text_0021:{
+  fontWeight:"bold",
+},
+div_1_3:{
+width:wp("90%"),
+minHeight:100,
+display:"flex",
+justifyContent:"space-around",
+marginVertical:40
+
+
+},
+div_1_4:{
+  display:"flex",
+  flexDirection:"column",
+  alignItems:"center",
+  // justifyContent:"space-between",
+  padding:20
+},
+image65:{
+  width:20,
+  height:10
+},
+Text_0022:{
+  color:"white",
+  fontWeight:"bold"
+},
+div_1_5:{
+  width:"90%",
+  display:"flex",
+  flexDirection:"row",
+  alignItems:"center",
+  justifyContent:"space-between",
+},
+Text_0023:{
+  color:"white",
+  fontWeight:"bold",
+  fontSize:25,
+
+  width:"96%"
+},
+Text_0024:{
+  color:"white",
+  fontWeight:"bold",
+  fontSize:17,
+  width:"90%"
+}
+
+
+})
